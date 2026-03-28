63853642caf85850c41e8e4237ef5dbe363575e9
mohit1278-mohit <choudharymohit12292@gmail.com>
2026-03-28 18:24:07 +0000

    Deploying to gh-pages from @ mohit1278-mohit/Pulsegrid@f37eee47b99026237f3bee8fb1c8e746d869a23a 🚀

---
+++ _next/static/hqhixTRIrrQzIXxZxSNy9/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pulsegrid//_next/:path+",
+        "destination": "/pulsegrid/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
